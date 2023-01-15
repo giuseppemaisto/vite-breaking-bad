335e3ed70dc282ed13e3373b09e606384e88f5f2
giuseppemaisto <gmaisto98@gmail.com>
2023-01-15 16:48:03 +0100

    header aggiunto

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
     <script>
+    import axios from 'axios'
+    import AppHeader from './components/AppHeader.vue';
+    import AppContent from './components/AppContent.vue';
+    import CharacterList from './components/CharacterList.vue'
+    import {store} from './store.js'
     export default {
+      components: {
+        AppHeader,
+        CharacterList,
+        AppContent
+      }, 
+      data(){
+        return {
+          store
+        }
+      },
+      created(){
+
+        this.getCardList()
+      },
+      methods: {
+        getCardList(){
+          axios.get(store.url).then((response) => {
+            store.cardsList = response.data.data
+          })
+        }
+      },
       
     }
     </script>
@@ -8,6 +34,6 @@
     
   </div>
 </template>
-<style lang="">
-  
+<style lang="scss">
+  @use '../src/assets/styles/generals.scss'
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,10 +4,20 @@ export default {
 }
 </script>
 <template lang="">
-<div>
-
-</div>
+<header>
+    <h1>OU-GI-OH Api</h1>
+</header>
 </template>
-<style lang="">
+<style lang="scss">
+header{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 1rem 0;
 
+    .header-content{
+        display: flex;
+        align-items: center;
+    }
+}
 </style>
